5c71e8c6b8b5d3e7412734ede3f734b414eaee94
Banjo Tola <banjotola12@gmail.com>
2022-03-02 20:54:09 +0100

    final touches

--- components/UpcomingRides.js
+++ components/UpcomingRides.js
@@ -11,70 +11,73 @@ function UpcomingRides({
   state_name,
 }) {
   return (
-    <Box p="20px" bg="#171717" mb="20px" borderRadius={"10px"}>
-      <Stack direction={["column", "row"]}>
-        <Box>
-          <Image  w={{base: "100%"}} src={map_pc} alt="Dan Abramov" />
-        </Box>
+    <Stack
+      flexWrap={"wrap"}
+      p="20px"
+      bg="#171717"
+      mb="20px"
+      borderRadius={"10px"}
+      direction={["column", "row"]}
+    >
+      <Box w={{ base: "100%", md: "250px" }}>
+        <Image w={{ base: "100%" }} src={map_pc} alt="Dan Abramov" />
+      </Box>
 
-        <Box pl={{base: "0px", md: "20px"}}>
-          <HStack mb="15px">
-            <Text color="#CFCFCF">Rate id:</Text>
-            <Text color="white">{id}</Text>
-          </HStack>
-          <HStack mb="15px">
-            <Text color="#CFCFCF">Origin Station:</Text>
-            <Text color="white">{origin_station}</Text>
-          </HStack>
-          <HStack mb="15px">
-            <Text color="#CFCFCF">station_path:</Text>
+      <Box pl={{ base: "0px", md: "20px" }}>
+        <HStack mb={{ base: "20px", md: "30px" }}>
+          <Text color="#CFCFCF">Rate id:</Text>
+          <Text color="white">{id}</Text>
+        </HStack>
+        <HStack mb={{ base: "20px", md: "30px" }}>
+          <Text color="#CFCFCF">Origin Station:</Text>
+          <Text color="white">{origin_station}</Text>
+        </HStack>
+        <HStack mb={{ base: "20px", md: "30px" }}>
+          <Text color="#CFCFCF">station_path:</Text>
 
-            <Text color="white">
-              [
-              {path.map((p) =>
-                p === path[path.length - 1] ? `${p}` : `${p}, `
-              )}
-              ]
-              {/* [
+          <Text color="white">
+            [
+            {path.map((p) => (p === path[path.length - 1] ? `${p}` : `${p}, `))}
+            ]
+            {/* [
             {path.map((p) =>
               p === path[path.length - 1] ? `${p}` : `${p}, `
             )}
             ] */}
-            </Text>
-          </HStack>
-          <HStack mb="15px">
-            <Text color="#CFCFCF">Date:</Text>
-            <Text color="white">{date}</Text>
-          </HStack>
-          <HStack>
-            <Text color="#CFCFCF">Distance :</Text>
-            <Text color="white">{distance}</Text>
-            {/* <Text color="white">{stationPath[0] - user.station_code}</Text> */}
-          </HStack>
-        </Box>
-        <Spacer />
-        <Box>
-          <HStack>
-            <Text
-              p="5px"
-              borderRadius={"16px"}
-              color="#FFFFFF"
-              border="1px solid #000000"
-            >
-              {city_name}
-            </Text>
-            <Text
-              p="5px"
-              borderRadius={"16px"}
-              color="#FFFFFF"
-              border="1px solid #000000"
-            >
-              {state_name}
-            </Text>
-          </HStack>
-        </Box>
-      </Stack>
-    </Box>
+          </Text>
+        </HStack>
+        <HStack mb={{ base: "20px", md: "30px" }}>
+          <Text color="#CFCFCF">Date:</Text>
+          <Text color="white">{date}</Text>
+        </HStack>
+        <HStack>
+          <Text color="#CFCFCF">Distance :</Text>
+          <Text color="white">{distance}</Text>
+          {/* <Text color="white">{stationPath[0] - user.station_code}</Text> */}
+        </HStack>
+      </Box>
+      <Spacer />
+      <Box pt={{ base: "20px", md: "0px" }}>
+        <HStack>
+          <Text
+            p="5px"
+            borderRadius={"16px"}
+            color="#FFFFFF"
+            border="1px solid #000000"
+          >
+            {city_name}
+          </Text>
+          <Text
+            p="5px"
+            borderRadius={"16px"}
+            color="#FFFFFF"
+            border="1px solid #000000"
+          >
+            {state_name}
+          </Text>
+        </HStack>
+      </Box>
+    </Stack>
   );
 }
 
