a9906b5bb862d14477cd88d6d59a1eba38cef355
Banjo Tola <banjotola12@gmail.com>
2022-03-02 17:30:34 +0100

    modal closer color

--- components/FilterModal.js
+++ components/FilterModal.js
@@ -15,24 +15,57 @@ import {
 } from "@chakra-ui/react";
 import React from "react";
 
-function FilterModal({ onOpen, onClose, isOpen, rides, onChangeState, onChangeCity, stateValue, cityValue }) {
+function FilterModal({
+  onOpen,
+  onClose,
+  isOpen,
+  rides,
+  onChangeState,
+  onChangeCity,
+  stateValue,
+  cityValue,
+}) {
   return (
     <>
       <Modal size={"xs"} isOpen={isOpen} onClose={onClose}>
         <ModalOverlay />
         <ModalContent bg="black">
-          <ModalCloseButton />
+          <ModalCloseButton bg="red" />
           <ModalBody mt="35px">
-            <Text  mb="10px" color="#A5A5A5">Filters</Text>
-            <Divider mb="10px"/>
-            <Select onChange={onChangeState} value={stateValue} bg={"#101010"} color="orange" mb="10px" placeholder="State">
+            <Text mb="10px" color="#A5A5A5">
+              Filters
+            </Text>
+            <Divider mb="10px" />
+            <Select
+              onChange={onChangeState}
+              value={stateValue}
+              bg={"#101010"}
+              color="orange"
+              mb="10px"
+              placeholder="State"
+            >
               {rides.map((ride, i) => {
-                return <option value={ride.state} key={i}>{ride.state}</option>;
+                return (
+                  <option value={ride.state} key={i}>
+                    {ride.state}
+                  </option>
+                );
               })}
             </Select>
-            <Select value={cityValue} onChange={onChangeCity} color={"orange"} bg="#101010" mb="10px" placeholder="City">
+            <Select
+              value={cityValue}
+              onChange={onChangeCity}
+              color={"orange"}
+              bg="#101010"
+              mb="10px"
+              placeholder="City"
+            >
               {rides.map((ride, i) => {
-                return <option value={ride.city} color="black" key={i}>{ride.city}</option>;
+                return (
+                  <option value={ride.city} color="black" key={i}>
+                    {ride.city}
+                  </option>
+                );
               })}
             </Select>
           </ModalBody>
